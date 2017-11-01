14176a75e0114ff9b52956e2d6dbf2947d06a21f
walkingzq <walkingzq@163.com>
2017-11-01 15:10:02 +0800

    项目初始化

---
+++ src/main/java/web/Application.java
@@ -0,0 +1,15 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by qaa on 2017/11/1.
+ * 工程主入口
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
